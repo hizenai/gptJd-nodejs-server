cf80faa620ebf421baeb9fbdfd31c64567365650
pranjalanimesh <pranjalanimesh5@gmail.com>
2024-02-01 23:01:18 +0530

    modified according the deployment

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ const port = process.env.PORT || 5000;
 
 app.use("/",gptJobDescriptionsRouter);
 
+app.use("/", (req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
 
 
+module.exports = app;
